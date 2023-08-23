99799b1f61d7f795869225272521a6762cd62ad9
nickglu86 <enayfls@gmail.com>
2023-08-23 14:25:12 +0300

    Products & Cart Features

--- src/components/ProductsGallery.tsx
+++ src/components/ProductsGallery.tsx
@@ -6,7 +6,8 @@ type Product = {
     name: string;
     image: { url: string };
     id: string
- 
+    price: { formatted_with_symbol : string}
+     
 };
 
 
@@ -33,9 +34,10 @@ const ProductsGallery: FC = () => {
       }
 
     const GetProduct: FC<{ product: Product}> = ({ product }) => (
-        <li key={product.id}>
+        <li key={product.id} >
             <h4>{product.name}</h4>
             <img src={product.image.url} width={250} alt={product.name} />
+            <div>{product.price.formatted_with_symbol}</div>
             <button onClick={ ()=> handleAddToCart(product.id, 1)}>Add to Cart</button>
         </li>
     );
